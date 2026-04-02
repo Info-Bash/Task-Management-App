c2c98f6b8226f0787b4b46b01b3d7b0b0e3a622b
Bash <abubakarbabatunde20@gmail.com>
2026-04-02 07:58:07 +0100

    fix refresh (bug)

--- task-management-system/vite.config.js
+++ task-management-system/vite.config.js
@@ -11,6 +11,12 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    historicFallback: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {
